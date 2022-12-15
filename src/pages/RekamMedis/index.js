6bbcfd51a9d84aa1aba5f1eacdd08bd268c113f4
Abdur Rahim <abdoerrahiem@gmail.com>
2022-12-15 10:14:55 +0700

    export excel

--- src/pages/RekamMedis/index.js
+++ src/pages/RekamMedis/index.js
@@ -7,13 +7,22 @@ import {
   Alert,
   ScrollView,
   StatusBar,
+  PermissionsAndroid,
 } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faPlus, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faPlus,
+  faUser,
+  faDownload,
+  faFileExcel,
+} from '@fortawesome/free-solid-svg-icons'
 import DaftarPasien from '../../components/molecules/DaftarPasien'
 import InputData from '../../components/atoms/InputData'
 import { Fire } from '../../config'
 import { connect } from 'react-redux'
+import { Button } from '../../components'
+const RNFS = require('react-native-fs')
+import XLSX from 'xlsx'
 
 class RekamMedis extends Component {
   constructor(props) {
@@ -122,8 +131,79 @@ class RekamMedis extends Component {
     }
   }
 
+  exportDataToExcel = () => {
+    // Created Sample data
+    let sample_data_to_export = [...this.props.pasienList]
+    sample_data_to_export.map((item, key) => {
+      sample_data_to_export[key].image = ''
+      sample_data_to_export[key].signature = ''
+      sample_data_to_export[key].id = key + 1
+    })
+
+    let wb = XLSX.utils.book_new()
+    let ws = XLSX.utils.json_to_sheet(sample_data_to_export)
+    XLSX.utils.book_append_sheet(wb, ws, 'Pasien')
+    const wbout = XLSX.write(wb, { type: 'binary', bookType: 'xlsx' })
+
+    const fileStorage =
+      RNFS.ExternalStorageDirectoryPath +
+      `/Download/pasien-${new Date().valueOf()}.xlsx`
+    RNFS.writeFile(fileStorage, wbout, 'ascii')
+      .then((r) => {
+        console.log('Success')
+        Alert.alert(
+          'Unduh Berhasil',
+          `File berhasil terunduh ke ${fileStorage}`,
+          [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
+        )
+      })
+      .catch((e) => {
+        console.log('Error', e)
+        Alert.alert(
+          'Download Gagal',
+          'Terjadi kesalahan saat mengunduh file anda.',
+          [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
+        )
+      })
+  }
+
+  handleExport = async () => {
+    try {
+      let isPermitedExternalStorage = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+      )
+
+      if (!isPermitedExternalStorage) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Storage permission needed',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          }
+        )
+
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          this.exportDataToExcel()
+          console.log('Permission granted')
+        } else {
+          // Permission denied
+          console.log('Permission denied')
+        }
+      } else {
+        this.exportDataToExcel()
+      }
+    } catch (e) {
+      console.log('Error while checking permission')
+      console.log(e)
+      return
+    }
+  }
+
   render() {
     const { pasiens } = this.state
+    const { profile, pasienList } = this.props
 
     return (
       <View style={styles.page}>
@@ -134,11 +214,13 @@ class RekamMedis extends Component {
         />
         <ScrollView showsVerticalScrollIndicator={false}>
           <Text style={styles.title}>Rekam Medis</Text>
+
           <InputData
             placeholder={'Cari Pasien'}
             placeholderColor={'#FFFFFF'}
             onChangeText={this.onSearchPatient}
           />
+
           <View>
             <View style={styles.wrapperUser}>
               <TouchableOpacity
@@ -156,7 +238,32 @@ class RekamMedis extends Component {
                   <Text style={styles.text}>TAMBAH PASIEN</Text>
                 </View>
               </TouchableOpacity>
+
+              {pasienList.length > 0 &&
+                profile?.premiumExpired &&
+                new Date() < new Date(profile?.premiumExpired) && (
+                  <TouchableOpacity
+                    style={styles.btnTambah}
+                    onPress={this.handleExport}
+                  >
+                    <FontAwesomeIcon
+                      icon={faFileExcel}
+                      size={20}
+                      color={'white'}
+                    />
+                    <View>
+                      <FontAwesomeIcon
+                        style={styles.plus}
+                        icon={faDownload}
+                        size={18}
+                        color={'white'}
+                      />
+                      <Text style={styles.text}>EKSPOR EXCEL</Text>
+                    </View>
+                  </TouchableOpacity>
+                )}
             </View>
+
             <View style={styles.wrapperScroll}>
               <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                 <View style={styles.category}>
